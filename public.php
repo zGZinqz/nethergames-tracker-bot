b9afcdb0a79ba569485799eebdd0daa2f352fa74
BariPlayzYT <rifatbari28@gmail.com>
2021-05-09 10:24:11 +0600

    better help command

--- public.php
+++ public.php
@@ -42,22 +42,22 @@ $bot->on('ready', function ($discord){
     $discord->on('message', function ($message, $discord) {
         $param = explode(' ', $message->content);
 
-        if($param[0] == "ng!track") {
+        if ($param[0] == "ng!track") {
 
             /*param 1 = what to track
              param 2 = ur ign idiot
              param 3 = time to track for
             */
-
             if (array_key_exists(1, $param)) {
                 if (array_key_exists(2, $param)) {
                     if (array_key_exists(3, $param)) {
+
                         if ($param[1] == "kills" or $param[1] == "bwWins" or $param[1] == "bwKills" or $param[1] == "wins" or $param[1] == "bwDeaths" or $param[1] == "deaths") {
                             $response = file_get_contents("https://apiv2.nethergames.org/players/" . $param[2] . "/stats");
                             $info = json_decode($response);
                             if (is_object($info)) {
-                                if(is_numeric($param[3])) {
-                                    if($param[3] > "300") {
+                                if (is_numeric($param[3])) {
+                                    if ($param[3] > "300") {
                                         $embed = new Embed($discord);
                                         $embed->setTitle("Success");
                                         $embed->setDescription("Now tracking your " . $param[1] . " stats on the IGN " . $param[2] . " for " . $param[3] . " seconds.\nMake sure your DMs are on so we can send your tracked stats in the time given!");
@@ -68,7 +68,7 @@ $bot->on('ready', function ($discord){
                                         var_dump($info, $response);
 
 
-                                        $GLOBALS['timer-'.$message->user->id] = $discord->getLoop()->addPeriodicTimer($param[3], function () use ($message, $param, $info, $discord) {
+                                        $GLOBALS['timer-' . $message->user->id] = $discord->getLoop()->addPeriodicTimer($param[3], function () use ($message, $param, $info, $discord) {
                                             $newresponse = file_get_contents("https://apiv2.nethergames.org/players/" . $param[2] . "/stats");
                                             $newinfo = json_decode($newresponse);
                                             $finalRes = $newinfo->{$param[1]} - $info->{$param[1]};
@@ -77,10 +77,10 @@ $bot->on('ready', function ($discord){
                                             $discord->getLoop()->cancelTimer($GLOBALS['timer-' . $message->user->id]);
                                         });
 
-                                    }else{
+                                    } else {
                                         $message->reply("The seconds counter cannot be less than 300 seconds (5 Minutes) because of nethergame's caching system. Please put a value higher than 300 seconds.");
                                     }
-                                }else{
+                                } else {
                                     $embed = new Embed($discord);
                                     $embed->setTitle("Error");
                                     $embed->setDescription("Please use numeric digits for how much time to track. (Specified in Seconds. E.G: ng!track bwWins thebarii 3)");
@@ -89,7 +89,7 @@ $bot->on('ready', function ($discord){
                                     $embed->setTimestamp();
                                     $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
                                 }
-                            } else{
+                            } else {
                                 $embed = new Embed($discord);
                                 $embed->setTitle("Error");
                                 $embed->setDescription("Something went wrong.\nPlease check the IGN for typos.");
@@ -100,7 +100,9 @@ $bot->on('ready', function ($discord){
                             }
 
 
-                        } else {
+                        }elseif($param[1] == "help") {
+                            $message->reply("**COMMANDS:**\nng!tracker help (this command)\n\nng!track param[1] param[2] param[3]\n\nng!source (get the source code)\n\n**USAGE:**\nparam[1] = What you want to track\nparam[2] = Your IGN\nparam[3] = numeric number above 300 which counts as seconds");
+                        }else {
                             $embed = new Embed($discord);
                             $embed->setTitle("Error");
                             $embed->setDescription("Not a valid game-statistic to track! Get a list of stats we can track using **ng!tracklist**");
@@ -136,12 +138,12 @@ $bot->on('ready', function ($discord){
                 $embed->setTimestamp();
                 $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
             }
-        }elseif($param[0] == "ng!tracklist"){
+        } elseif ($param[0] == "ng!tracklist") {
             $message->reply("We can only track these statistics at the moment. Capitalizations must be used.\n\n**LIST:**\nbwKills\nbwDeaths\nbwWins\nwins\nkills\ndeaths");
-        }elseif($param[0] == "ng!source"){
+        } elseif ($param[0] == "ng!source") {
             $message->reply("My source code can be found at https://github.com/BariPlayzYT/nethergames-tracker-bot on github written in PHP by Bari!");
-        }elseif($message->content == "ng!tracker help"){
-            $message->reply("**COMMANDS:**\nng!tracker help (this command)\nng!track [What To Track, use ng!tracklist to see what you can track.] [IGN] [Time to track for (in seconds)]\nng!source (get the source code)");
+        } elseif ($message->content == "ng!tracker help") {
+            $message->reply("**COMMANDS:**\nng!tracker help (this command)\n\nng!track param[1] param[2] param[3]\n\nng!source (get the source code)\n\n**USAGE:**\nparam[1] = What you want to track\nparam[2] = Your IGN\nparam[3] = numeric number above 300 which counts as seconds");
         }
     });
 
