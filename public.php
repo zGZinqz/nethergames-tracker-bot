dac1716497d2d57ce338e2dd020157343ae1d574
BariPlayzYT <rifatbari28@gmail.com>
2021-05-09 11:59:03 +0600

    reformat (thanks to AdminRAT)

--- public.php
+++ public.php
@@ -22,21 +22,22 @@ $bot->on('ready', function ($discord){
     $discord->on(Event::MESSAGE_CREATE, function ($message){
         $param = explode(' ', $message->content);
 
-        /*if($param[0] == "!stats"){
+        /*
+          if($param[0] == "!stats"){
                 if(!is_null($param[1])) {
                     echo "{$message->author->username} ran {$message->content}";
                     $response = file_get_contents("https://apiv2.nethergames.org/players/".$param[1]."/stats");
                     $info = json_decode($response);
                     if(is_object($info)){
                         $message->reply($param[1]."'s wins: " . $info->wins);
-                    }else{
+                    } else {
                         $message->reply("Something went wrong.\nPlease check the IGN for typos.");
                     }
-                }else{
+                } else{
                     $message->reply("Please input an username.");
             }
-
-        }*/
+        }
+        */
     });
 
     $discord->on('message', function ($message, $discord) {
@@ -44,10 +45,12 @@ $bot->on('ready', function ($discord){
 
         if ($param[0] == "ng!track") {
 
-            /*param 1 = what to track
-             param 2 = ur ign idiot
-             param 3 = time to track for
+            /*
+              param 1 = Stat to track
+              param 2 = Player's Xbox tag
+              param 3 = Amount of time to track
             */
+
             if (array_key_exists(1, $param)) {
                 if (array_key_exists(2, $param)) {
                     if (array_key_exists(3, $param)) {
@@ -60,7 +63,7 @@ $bot->on('ready', function ($discord){
                                     if ($param[3] > "300") {
                                         $embed = new Embed($discord);
                                         $embed->setTitle("Success");
-                                        $embed->setDescription("Now tracking your " . $param[1] . " stats on the IGN " . $param[2] . " for " . $param[3] . " seconds.\nMake sure your DMs are on so we can send your tracked stats in the time given!");
+                                        $embed->setDescription("Now tracking your " . $param[1] . " stats for **" . $param[2] . "** for **" . $param[3] . "** seconds.\nMake sure your DMs are enabled/allowed so I can send your tracked stats after the time is up!");
                                         $embed->setColor("#16FF00");
                                         $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                                         $embed->setTimestamp();
@@ -73,85 +76,76 @@ $bot->on('ready', function ($discord){
                                             $newinfo = json_decode($newresponse);
                                             $finalRes = $newinfo->{$param[1]} - $info->{$param[1]};
                                             $id = $message->author->id;
-                                            $message->user->sendMessage("<@$id> We successfully tracked your stats!\n\n**INFO:**\nFinal Results of Tracked Stats\nGained " . $param[1] . ": " . $finalRes . "\n\n**Other Info:**\nIGN: " . $param[2] . "\nTime tracked for: " . $param[3] . " seconds.\nOld Stats: " . $info->{$param[1]} . "\nNew stats: " . $newinfo->{$param[1]});
+                                            $message->user->sendMessage("<@$id> We successfully tracked your stats!\n\n**INFO:**\nFinal results of tracked stats:\nGained " . $param[1] . ": " . $finalRes . "\n\n**Other Info:**\nIGN: " . $param[2] . "\nTime tracked for: " . $param[3] . " seconds.\nOld Stats: " . $info->{$param[1]} . "\nNew stats: " . $newinfo->{$param[1]});
                                             $discord->getLoop()->cancelTimer($GLOBALS['timer-' . $message->user->id]);
                                         });
-
-                                    } else {
-                                        $message->reply("The seconds counter cannot be less than 300 seconds (5 Minutes) because of nethergame's caching system. Please put a value higher than 300 seconds.");
+                                    } else { // User sends time less than 300 seconds
+                                        $message->reply("The seconds counter cannot be less than 300 seconds (5 minutes) because of NetherGames' caching system. Please put a time higher than 300.");
                                     }
-                                } else {
+                                } else { // Time is NaN (Not a Number)
                                     $embed = new Embed($discord);
                                     $embed->setTitle("Error");
-                                    $embed->setDescription("Please use numeric digits for how much time to track. (Specified in Seconds. E.G: ng!track bwWins thebarii 3)");
+                                    $embed->setDescription("The time doesn't seem like a valid number. Please specify the time you want to track **in seconds**.");
                                     $embed->setColor("#FF0000");
                                     $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                                     $embed->setTimestamp();
                                     $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
                                 }
-                            } else {
+                            } else { // User specifies an invalid Xbox tag or something else went wrong internally
                                 $embed = new Embed($discord);
                                 $embed->setTitle("Error");
-                                $embed->setDescription("Something went wrong.\nPlease check the IGN for typos.");
+                                $embed->setDescription("Either you have specified an invalid Xbox tag, or something else went wrong. (Check if you made any typos in the IGN?)");
                                 $embed->setColor("#FF0000");
                                 $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                                 $embed->setTimestamp();
                                 $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
                             }
 
-
-                        }elseif($param[1] == "help") {
-                            $message->reply("**COMMANDS:**\nng!tracker help (this command)\n\nng!track param[1] param[2] param[3]\n\nng!source (get the source code)\n\n**USAGE:**\nparam[1] = What you want to track\nparam[2] = Your IGN\nparam[3] = numeric number above 300 which counts as seconds");
-                        }else {
+                        }else { // User specifies an invalid stat
                             $embed = new Embed($discord);
                             $embed->setTitle("Error");
-                            $embed->setDescription("Not a valid game-statistic to track! Get a list of stats we can track using **ng!tracklist**");
+                            $embed->setDescription("Not a valid game stat to track! Get a list of stats by running: `ng!tracklist`");
                             $embed->setColor("#FF0000");
                             $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                             $embed->setTimestamp();
                             $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
                         }
-                    } else {
+                    } else { // User does not specify a time
                         $embed = new Embed($discord);
                         $embed->setTitle("Error");
-                        $embed->setDescription("You must specify how long you want to track for (Seconds)! Get a list of stats we can track using **ng!tracklist**");
+                        $embed->setDescription("You must specify the length of time to track!");
                         $embed->setColor("#FF0000");
                         $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                         $embed->setTimestamp();
                         $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
                     }
-                } else {
+                } else { // User does not specify a Xbox tag
                     $embed = new Embed($discord);
                     $embed->setTitle("Error");
-                    $embed->setDescription("You must put your IGN! Get a list of stats we can track using **ng!tracklist**");
+                    $embed->setDescription("You did not specify an Xbox tag!");
                     $embed->setColor("#FF0000");
                     $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                     $embed->setTimestamp();
                     $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
                 }
-            } else {
+            } else { // User does not specify a stat to track
                 $embed = new Embed($discord);
                 $embed->setTitle("Error");
-                $embed->setDescription("You must specify what mode you want to track! Get a list of stats we can track using **ng!tracklist**");
+                $embed->setDescription("You must specify what stat you want to track! To see the list run: `ng!tracklist`");
                 $embed->setColor("#FF0000");
                 $embed->setFooter($message->author->username . ", Bot made by Bari", $av = $message->user->avatar);
                 $embed->setTimestamp();
                 $message->channel->sendMessage("<@" . $message->user->id . ">", false, $embed);
             }
-        } elseif ($param[0] == "ng!tracklist") {
-            $message->reply("We can only track these statistics at the moment. Capitalizations must be used.\n\n**LIST:**\nbwKills\nbwDeaths\nbwWins\nwins\nkills\ndeaths");
-        } elseif ($param[0] == "ng!source") {
-            $message->reply("My source code can be found at https://github.com/BariPlayzYT/nethergames-tracker-bot on github written in PHP by Bari!");
-        } elseif ($message->content == "ng!tracker help") {
-            $message->reply("**COMMANDS:**\nng!tracker help (this command)\n\nng!track param[1] param[2] param[3]\n\nng!source (get the source code)\n\n**USAGE:**\nparam[1] = What you want to track\nparam[2] = Your IGN\nparam[3] = numeric number above 300 which counts as seconds");
+        } else if ($param[0] == "ng!tracklist") { // tracklist command
+            $message->reply("We can only track these statistics at the moment. These are **CaSe sensitive**.\n```bwKills\nbwDeaths\nbwWins\nwins\nkills\ndeaths```");
+        } else if ($param[0] == "ng!source") { // source command
+            $message->reply("The source code can be found at:\nhttps://github.com/BariPlayzYT/nethergames-tracker-bot");
+        } else if ($message->content == "ng!tracker help") { // help command
+            $message->reply("**COMMANDS:**\n`ng!track [stat*] [Xbox tag] [time**]` - Track a user for a certain amount of time\n\n`ng!source` - Sends a link to the source code\n\n\* To see the list of stats to track, run `ng!tracklist`\n** Time MUST be in seconds, and at least 301 (5 minutes and 1 second)");
         }
     });
 
 });
 
-
 $bot->run();
-
-
-
-
